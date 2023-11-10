2e2e95c34b29ac3158a367655da6f6ac8f0036d2
Ayushghagre <148415018+Ayushghagre@users.noreply.github.com>
2023-11-10 16:05:08 +0530

    Update CreateList.java

--- CreateList.java
+++ CreateList.java
@@ -57,8 +57,7 @@ public class CreateList {
         cl.add(2);  
         cl.add(3);  
         cl.add(4);  
-        cl.add(5);
-        cl.add(6);
+       
         //Displays all the nodes present in the list  
         cl.display();  
     }  
